17b016df607cdfdf95b7f64c6f7cd8ebf7381ed1
GuruMahan <iosdeve2022@gmail.com>
2023-02-07 10:25:51 +0530

    Initial Commit

---
+++ TimerViewPOC/TimerViewPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerViewPOCApp.swift
+//  TimerViewPOC
+//
+//  Created by Guru Mahan on 07/02/23.
+//
+
+import SwiftUI
+
+@main
+struct TimerViewPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
